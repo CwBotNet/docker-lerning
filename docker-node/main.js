b55c15ed354147830e3091e120768d774a8f6c7d
Raj sahani <Rajsahaniofficial19@gmail.com>
2024-01-11 21:47:28 +0530

    main.js modified

--- docker-node/main.js
+++ docker-node/main.js
@@ -13,5 +13,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`connection successful on PORT: ${PORT}`);
+  console.log(`connection in a Docker container successful on PORT: ${PORT}`);
 });
